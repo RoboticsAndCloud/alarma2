4cc9d246ae05d63b761e93ea45e74c56224fde9a
petemadsen <petemadsen@freenet.de>
2017-07-26 05:26:18 +0200

    Added bt and cmd modules.

--- alarma2.ino
+++ alarma2.ino
@@ -13,10 +13,13 @@ void setup()
     Serial.println(F("[keypad] done"));
 
     mp3_setup();
-    Serial.println("F[mp3] done");
+    Serial.println(F("[mp3] done"));
 
     sonic_setup();
-    Serial.println("F[sonic] done");
+    Serial.println(F("[sonic] done"));
+
+    bt_setup();
+    Serial.println(F("[bt] done"));
 }
 
 
@@ -25,4 +28,13 @@ void loop()
     keypad_run();
 
     sonic_run();
+
+    bt_run();
+
+    if(Serial.available())
+    {
+    //if(Serial.read() == 'm')
+      //sound_play_debug(2);
+        cmd_add_char(Serial);
+    }
 }
---
+++ bt.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+#include "common.h"
+
+
+//SoftwareSerial bt_serial(BT_PIN_TX, BT_PIN_RX);
+
+
+void bt_setup()
+{
+//  myBTSerial.begin(BT_BAUD);
+}
+
+
+void bt_run()
+{
+/*
+    if (bt_serial.available())
+    {
+        cmd_add_char(bt_serial);
+    }
+*/
+}
+
