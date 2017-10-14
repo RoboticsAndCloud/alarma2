cc51d108d970e3200728d94dcd3e95be01f64857
petemadsen <petemadsen@freenet.de>
2017-10-14 14:30:46 +0200

    esp32: disabled DHCP

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -71,6 +71,15 @@ void app_main(void)
     nvs_flash_init();
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+
+	tcpip_adapter_dhcpc_stop(TCPIP_ADAPTER_IF_STA); // no DHCP
+
+	tcpip_adapter_ip_info_t ipInfo;
+	inet_pton(AF_INET, CONFIG_ADDRESS, &ipInfo.ip);
+	inet_pton(AF_INET, CONFIG_GATEWAY, &ipInfo.gw);
+	inet_pton(AF_INET, CONFIG_NETMASK, &ipInfo.netmask);
+	tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_STA, &ipInfo);
+
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
